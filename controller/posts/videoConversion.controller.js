087dadd0c2c95fc511d1e003a40181eb95413d4b
saimrahdari <steamsaim22@gmail.com>
2024-06-26 15:57:03 +0500

    Revert "Update videoConversion.controller.js"
    This reverts commit dca93227794767f78e81c1df37d3b585e9b01d98.

--- controller/posts/videoConversion.controller.js
+++ controller/posts/videoConversion.controller.js
@@ -7,15 +7,7 @@ const videoConversionController = {
     console.log("in change format");
     try {
       // next();
-res.header("Access-Control-Allow-Origin", "*");
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-      );
-      res.header(
-        "Access-Control-Allow-Methods",
-        "PUT, POST, GET, DELETE, OPTIONS"
-      );
+
       console.log("in try");
       const inputFilePath = req.body.file; // Use req.file.path provided by multer
       const format = req.body.format;
@@ -63,15 +55,6 @@ res.header("Access-Control-Allow-Origin", "*");
 
   async changeCodec(req, res) {
     try {
-      res.header("Access-Control-Allow-Origin", "*");
-      res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-      );
-      res.header(
-        "Access-Control-Allow-Methods",
-        "PUT, POST, GET, DELETE, OPTIONS"
-      );
       const { file, codec } = req.body;
       console.log(req.body);
       await processVideo(file, codec, res);
