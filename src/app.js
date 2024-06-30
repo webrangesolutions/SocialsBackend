67ec0f0d8aa6ff152543193a52918714375ecb05
Saim Rahdari <43989990+saimrahdari@users.noreply.github.com>
2024-06-30 13:53:24 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const session = require("express-session");
 const path = require("path");
 const ffmpeg = require('fluent-ffmpeg');
 
-ffmpeg.setFfmpegPath('/usr/ffmpeg-7/ffmpeg-7');
+ffmpeg.setFfmpegPath('/usr/bin/ffmpeg');
 // // Set the paths for ffmpeg and ffprobe
 // const ffmpegPath = path.resolve('/usr/bin/ffmpeg/ffmpeg.exe');
 // const ffprobePath = path.resolve('/usr/bin/ffmpeg/ffprobe.exe');
