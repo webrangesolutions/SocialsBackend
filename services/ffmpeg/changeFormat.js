76a5b1317d12cc1e66da40c1b255d6f232a119b3
Muhammad Touseef <muhammadtouseefmughal@gmail.com>
2024-06-30 08:54:16 -0700

    Update changeFormat.js

--- services/ffmpeg/changeFormat.js
+++ services/ffmpeg/changeFormat.js
@@ -50,6 +50,9 @@ const changeFormat = async (file, format, scanType,res) => {
     console.log("file is ...", file, "... format is ...", format," ... ")
     const fileName = extractFileName(file).split("/")[1];
     const localFilePath = path.join(__dirname, "temp", fileName);
+   const tempTrimDir = await fs.promises.mkdtemp(path.join(fs.realpathSync('.'), 'trimmedVideo-'));
+    const localFilePath = path.join(tempTrimDir, "trimmedVideo.mp4");
+
     const outputFilePath = path.join(__dirname, "temp", `output.${format}`);
 
     // console.log("local...",localFilePath,"...output...", outputFilePath )
